23df39bea765022f8ba407790f05461adaa832b6
Neda Sultova <neda.sultova@gmail.com>
2021-01-25 12:53:47 +0100

    changed docs name

--- gitcrawl/docs/conf.py
+++ gitcrawl/docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'gitcrawl-docs'
+project = 'gitcrawl'
 copyright = '2021, Neda Sultova'
 author = 'Neda Sultova'
 
